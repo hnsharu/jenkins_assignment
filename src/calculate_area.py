3a35475361be3e9ac92ae447f96c9924acc8d770
sharuu <sharulhn43@gmail.com>
2024-03-09 10:03:30 +0530

    initial commit

---
+++ src/calculate_area.py
@@ -0,0 +1,5 @@
+import math
+
+#circle area calculating function rounding to 2 decimal places
+def cal_area(radius):
+    return round(math.pi*(radius**2),2)
---
+++ tests/test_cal_area.py
@@ -0,0 +1,7 @@
+from calculate_area import cal_area
+
+def test_cal_area():
+    assert cal_area(2)==12.57
+    assert cal_area(5)==78.54
+    assert cal_area(5)==78.54
+    assert cal_area(2)==12.57 
